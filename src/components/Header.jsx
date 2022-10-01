ac0d46873a348a6a454a28f5ff0a4b1fdf17f7df
Nabi94 <nebi9447@gmail.com>
2022-10-01 16:40:31 +0400

    folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import '../styles/main.scss';
+
+
+
+const Header = ()=>{
+    return ( <header className='header'>
+        <div className="container">
+            <div className="header__logo">
+            <p className='myname'>salam</p>
+            
+            </div>
+        </div>
+    </header>
+    )
+}
+export default Header;
